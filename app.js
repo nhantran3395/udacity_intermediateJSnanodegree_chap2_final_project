94e8a34871e8542208cc218a9bef18a32a36ca02
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2021-03-20 16:10:19 +0700

    feat: create infographic showing user and dinosaurs
    prepare human, birdm dinosaurs data

--- app.js
+++ app.js
@@ -1,34 +1,55 @@
 console.log("dinosaur rocks!")
 
-const human = (function () {
-  let _name
-  let _height
-  let _weight
-  let _diet
+const MAX_NUM_DINO_FACT = 6
 
-  function getName() {
-    return _name
-  }
+function Dinosaur(animal) {
+  let { species, weight, height, diet, where, when, fact } = animal
+  let _facts = new Array(MAX_NUM_DINO_FACT)
 
-  function getHeight() {
-    return _height
-  }
-
-  function getWeight() {
-    return _weight
-  }
-
-  function getDiet() {
-    return _diet
-  }
+  _facts[0] = fact
 
   return {
-    name: getName(),
-    height: getHeight(),
-    weight: getWeight(),
-    diet: getDiet(),
+    species: species,
+    weight: weight,
+    height: height,
+    diet: diet,
+    where: where,
+    when: when,
+    facts: _facts,
   }
-})()
+}
+
+function Bird(animal) {
+  let { species, weight, height, diet, where, when, fact } = animal
+
+  return {
+    species: species,
+    weight: weight,
+    height: height,
+    diet: diet,
+    where: where,
+    when: when,
+    fact: fact,
+  }
+}
+
+function Human(name, heightFeet, heightInches, weight, diet) {
+  let height = (function getUserHeightInFeet(heightFeet, heightInches) {
+    const inchToFeetMultiplier = 0.0833333333
+
+    return (
+      Number(heightFeet) +
+      Number((heightInches * inchToFeetMultiplier).toFixed(1))
+    )
+  })(heightFeet, heightInches)
+
+  return {
+    name: name,
+    height: height,
+    weight: weight,
+    diet: diet,
+  }
+}
 
 const formUserInfo = document.forms["user-info"]
 
@@ -38,10 +59,29 @@ formUserInfo.addEventListener("submit", function (event) {
   const entries = formData.entries()
   const data = Object.fromEntries(entries)
 
-  human.name = data["user-name"]
-  human.height = data["user-height-feet"]
-  human.weight = data["user-weight"]
-  human.diet = data["user-diet"]
+  const userName = data["user-name"]
+  const userHeightFeet = data["user-height-feet"]
+  const userHeightInches = data["user-height-inches"]
+  const userWeight = data["user-weight"]
+  const userDiet = data["user-diet"]
+
+  const human = Human(
+    userName,
+    userHeightFeet,
+    userHeightInches,
+    userWeight,
+    userDiet
+  )
+
+  const dinosaurs = dinos
+    .filter((dino) => dino.species != "Pigeon")
+    .map((dino) => Dinosaur(dino))
+
+  const bird = Bird(dinos.find((dino) => dino.species == "Pigeon"))
+
+  console.log(human)
+  console.log(dinosaurs)
+  console.log(bird)
 
   formUserInfo.remove()
   document.getElementById("form-user-try-again").style.removeProperty("display")
